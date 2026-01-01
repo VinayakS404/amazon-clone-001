2cefefa0ff99b282ba40505516a5b118ce016473
Vinayak S <oreva.vinayak@gmail.com>
2026-01-01 22:09:46 +0530

    saved cell data

---
+++ data/data.js
@@ -0,0 +1,113 @@
+const cellProductsData = [{
+  title : 'Revamp your home in style',
+  imgs : {
+    img1 : '../img/table1-img1.jpg',
+    img2 : '../img/table1-img2.jpg',
+    img3 : '../img/table1-img3.jpg',
+    img4 : '../img/table1-img4.jpg'
+  },
+  infos : {
+    info1 : 'Cushion covers, bedsheets & more',
+    info2 : 'Figurines, vases & more',
+    info3 : 'Home storage',
+    info4 : 'Lighting solutions',
+  }
+},{
+  title : 'Bulk order discounts + Up to 18% GST savings',
+  imgs : {
+    img1 : '../img/table2-img1.jpg',
+    img2 : '../img/table2-img2.jpg',
+    img3 : '../img/table2-img3.jpg',
+    img4 : '../img/table2-img4.jpg'
+  },
+  infos : {
+    info1 : 'Up to 45% off | Laptops',
+    info2 : 'Up to 60% off | Kitchen appliances',
+    info3 : 'Min. 50% off | Office furniture',
+    info4 : 'Up to 60% off | for Business Purchases',
+  }
+},{
+  title : 'Starting ₹49 | Deals on home essentials',
+  imgs : {
+    img1 : '../img/table3-img1.jpg',
+    img2 : '../img/table3-img2.jpg',
+    img3 : '../img/table3-img3.jpg',
+    img4 : '../img/table3-img4.jpg'
+  },
+  infos : {
+    info1 : 'Cleaning supplies',
+    info2 : 'Bathroom accessories',
+    info3 : 'Home tools',
+    info4 : 'Wallpapers',
+  }
+},{
+  title : 'Starting ₹149 | Headphones',
+  imgs : {
+    img1 : '../img/table4-img1.jpg',
+    img2 : '../img/table4-img2.jpg',
+    img3 : '../img/table4-img3.jpg',
+    img4 : '../img/table4-img4.jpg'
+  },
+  infos : {
+    info1 : 'Starting ₹249 | boAt',
+    info2 : 'Starting ₹349 | boult',
+    info3 : 'Starting ₹649 | Noise',
+    info4 : 'Starting ₹149 | Zebronics',
+  }
+},{
+  title : 'Automotive essentials | Up to 60% off',
+  imgs : {
+    img1 : '../img/table5-img1.jpg',
+    img2 : '../img/table5-img2.jpg',
+    img3 : '../img/table5-img3.jpg',
+    img4 : '../img/table5-img4.jpg'
+  },
+  infos : {
+    info1 : 'Cleaning accessories',
+    info2 : 'Tyre & rim care',
+    info3 : 'Helmets',
+    info4 : 'Vacuum cleaner',
+  }
+},{
+  title : 'Starting ₹199 | Amazon Brands & more',
+  imgs : {
+    img1 : '../img/table6-img1.jpg',
+    img2 : '../img/table6-img2.jpg',
+    img3 : '../img/table6-img3.jpg',
+    img4 : '../img/table6-img4.jpg'
+  },
+  infos : {
+    info1 : 'Starting ₹199 | Bedsheets',
+    info2 : 'Starting ₹199 | Curtains',
+    info3 : 'Minimum 40% off | Ironing board & more',
+    info4 : 'Up to 60% off | Home decor',
+  }
+},{
+  title : 'Up to 50% off | Baby care & toys | Amazon Brands',
+  imgs : {
+    img1 : '../img/table7-img1.jpg',
+    img2 : '../img/table7-img2.jpg',
+    img3 : '../img/table7-img3.jpg',
+    img4 : '../img/table7-img4.jpg'
+  },
+  infos : {
+    info1 : 'Up to 50% off | Baby diapers & wipes',
+    info2 : 'Up to 50% off | Ride ons',
+    info3 : 'Starting ₹649 | RC cars',
+    info4 : 'Up to 50% off | Baby safety essentials',
+  }
+},{
+  title : 'Appliances for your home | Up to 55% off',
+  imgs : {
+    img1 : '../img/table8-img1.jpg',
+    img2 : '../img/table8-img2.jpg',
+    img3 : '../img/table8-img3.jpg',
+    img4 : '../img/table8-img4.jpg'
+  },
+  infos : {
+    info1 : 'Air conditioners',
+    info2 : 'Refrigerators',
+    info3 : 'Microwaves',
+    info4 : 'Washing machines',
+  }
+}]
